60c377bbd26f0548168898ae32cf59b25ede62ea
Chitrakar <nideshchitrakar@bennington.edu>
2015-03-03 13:00:45 -0500

    Initial commit

---
+++ src/edu/bennington/cs/Main.java
@@ -0,0 +1,10 @@
+package edu.bennington.cs;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+    }
+}
